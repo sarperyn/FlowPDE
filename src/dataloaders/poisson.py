261448ead76cfe1ec67c77981de34697013c09c5
sarperyn <sarperyn@gmail.com>
2025-10-16 00:27:04 +0200

    poisson dataloader

--- src/dataloaders/poisson.py
+++ src/dataloaders/poisson.py
@@ -7,12 +7,12 @@ class PoissonDataset(Dataset):
     def __init__(self, data_path):
         
         data = loadmat(data_path)
-        phi = data["phi_data"].astype("float32")  # (N, 64, 64)
+        phi = data["phi_data"].astype("float32") 
         f   = data["f_data"].astype("float32")
 
         # Always normalize the data
-        u_mean, u_std = phi.mean(), phi.std() + 1e-8
-        f_mean, f_std = f.mean(),   f.std() + 1e-8
+        u_mean, u_std = phi.mean(), phi.std() #+ 1e-8
+        f_mean, f_std = f.mean(),   f.std() #+ 1e-8
         phi = (phi - u_mean) / u_std
         f   = (f   - f_mean) / f_std
         
@@ -21,4 +21,6 @@ class PoissonDataset(Dataset):
         
     def __len__(self): return self.u.shape[0]
 
-    def __getitem__(self, index): return self.u[index], self.f[index]
+    def __getitem__(self, index):
+
+        return {"u":self.u[index], "f":self.f[index]}
