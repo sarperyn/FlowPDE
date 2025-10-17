5e10949af6161c61a76a2684d888d2f44b2688bf
sarperyn <sarperyn@gmail.com>
2025-10-17 22:49:42 +0200

    generic test function added

--- tests/generic_test.py
+++ tests/generic_test.py
@@ -1,20 +1,15 @@
 import torch
 import os
 import sys
-import yaml
-#sys.path.append(os.path.dirname(os.getcwd()))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 from glob import glob
-from torch.utils.data import Dataset, DataLoader
-
-from src.models.mlp import MLP
-from src.trainers.flow_matching import FlowMatchingTrainer
-from src.datasets.poisson import PoissonDataset
 from utils.utils import load_config, load_class
 
-
+"""
+GENERIC TEST SCRIPT
+For a given .yaml file it can run experiments
+"""
 
 
 if __name__ == "__main__":
@@ -26,7 +21,7 @@ if __name__ == "__main__":
     config_path = os.path.join(project_dir, "configs", "poisson.yaml")
     assert os.path.exists(config_path), f"Config file not found at {config_path}"
 
-    #Parse config and use it to set up everything
+    # Parse config and use it to set up everything
     config = load_config(config_path)
     print(config)
     print("Config loaded successfully.")
@@ -39,29 +34,29 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    # --- 1. Dataset ---
+    # Dataset
     DatasetClass = load_class(config["dataset_config"]["class"])
     dataset = DatasetClass(glob(os.path.join(project_dir, config["data_dir"],f"*{config["spatial_dim"]}*"))[0])
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=config["trainer_config"]["batch_size"], shuffle=True)
     print("Dataset loaded.")
 
-    # --- 2. Model ---
+    # Model
     ModelClass = load_class(config["model_config"]["class"])
     model = ModelClass(input_dim=config["spatial_dim"] ** 2, hidden_dim=256)
     print("Model initialized.")
 
-    # --- 3. Optimizer ---
+    # Optimizer
     optimizer_cfg = config["optimizer_config"]
     OptimizerClass = load_class(optimizer_cfg["class_path"])
     optimizer = OptimizerClass(model.parameters(), **optimizer_cfg["init_args"])
     print("Optimizer set.")
 
-    # --- 4. Scheduler (optional) ---
+    # Scheduler
     SchedulerClass = load_class(config["scheduler_config"]["class"])
     scheduler = SchedulerClass(optimizer, T_0=10)
     print("Scheduler ready.")
 
-    # --- 5. Trainer ---
+    # Trainer
     TrainerClass = load_class(config["trainer_config"]["class"])
     trainer = TrainerClass(
         model=model,
@@ -71,7 +66,8 @@ if __name__ == "__main__":
     )
     print("Trainer created.")
 
-    print("🚀 Starting training...")
+    # Start traning
+    print("Starting training...")
     trainer.train(
         data_loader=dataloader,
         epochs=config['trainer_config']['epochs'],
